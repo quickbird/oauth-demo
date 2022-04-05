ee4609d0474444845d031c363f79db5ff08f104d
quickbird <quickbird0000@gmail.com>
2022-04-05 21:58:10 +0800

    初始化提交

---
+++ src/main/java/com/example/oauth/oauthdemo/controller/IndexController.java
@@ -0,0 +1,39 @@
+package com.example.oauth.oauthdemo.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.http.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class IndexController {
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/redirect")
+    public String redirect(String code, ModelMap modelMap){
+        HttpHeaders headers = new HttpHeaders();
+        List<MediaType> list = new ArrayList();
+        list.add(MediaType.APPLICATION_JSON);
+        headers.setAccept(list);
+        JSONObject param = new JSONObject();
+        param.put("client_id", "xxx");
+        param.put("client_secret", "xxx");
+        param.put("code", code);
+        HttpEntity httpEntity = new HttpEntity(param, headers);
+        ResponseEntity<JSONObject> responseEntity = restTemplate.postForEntity("https://github.com/login/oauth/access_token?client_id=9e1c1a4e702002be6a70&" +
+                "client_secret=962952bad9e3432bc03f4efbb8ae3baa6daaca8f&code=" + code, httpEntity, JSONObject.class);
+        headers.setBearerAuth(responseEntity.getBody().getString("access_token"));
+        httpEntity = new HttpEntity(null, headers);
+        ResponseEntity<JSONObject> resEntity = restTemplate.exchange("https://api.github.com/user", HttpMethod.GET, httpEntity, JSONObject.class);
+        modelMap.put("userName", resEntity.getBody().getString("login"));
+        return "welcome";
+    }
+}
